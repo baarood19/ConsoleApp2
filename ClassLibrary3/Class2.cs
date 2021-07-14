e72eb10a1ec628b0718536e32645cfa4e8f9d1d3
Kripal Parsekar <krupalparsekar3@gmail.com>
2021-07-14 10:58:26 +0530

    First Message

---
+++ ClassLibrary3/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class Class2
+    {
+        public string method2()
+        {
+            return "from class 2";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using ClassLibrary3;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl1 = new Class1();
+            Class2 cl2 = new Class2();
+            Console.WriteLine("Hello World!!!!!!");
+            Console.WriteLine(cl1.Method1());
+            Console.WriteLine(cl2.method2());
+            Thread.Sleep(TimeSpan.FromSeconds(4));
+        }
+    }
+}
